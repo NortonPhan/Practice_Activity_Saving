c49fb02ec89197096aef1fb83fccce4fb9385d0f
NortonPhan <82856263+NortonPhan@users.noreply.github.com>
2021-10-08 19:43:04 -0700

    Delete activity2.js

--- activity2.js
+++
@@ -1,9 +0,0 @@
-function back()
-{
-    window.location = "activity_1.html";
-}
-function get_score()
-{
-    var score = localStorage.getItem("score");
-    document.getElementById("updated_score").innerHTML = "<h1> Score: " + score + "</h1>";
-}
